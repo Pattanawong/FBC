fbac2d5fcd95c563a3b69c286b33c2953a3f778c
Pattanawong <166382461+Pattanawong@users.noreply.github.com>
2024-06-11 02:47:05 +0700

    F47

--- App/src/index.js
+++ App/src/index.js
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
-  <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph.</p>
-  </div>,
-  document.getElementById("root")
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
