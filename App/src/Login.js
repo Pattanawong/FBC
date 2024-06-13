5d3d62f0b3b22115eddecd4f42c2015f54fecb5a
Pattanawong <166382461+Pattanawong@users.noreply.github.com>
2024-06-14 03:46:58 +0700

    14d0346

--- App/src/Login.js
+++ App/src/Login.js
@@ -5,20 +5,27 @@ import { Link } from "react-router-dom";
 
 
 
-function  Login() {
-    return (
-      <div className="Login">
-        <form className="formlogin">
-            <input  type="email" placeholder="E-mail" />
-            <br />
-            <input type="password" placeholder="Password" />
-            <br />
-            <button >
-               <Link to='/Main'>Login</Link>  
-            </button>
-        </form>
-      </div>
-    );
-  }
-  
-  export default Login;
+function Login() {
+ 
+
+
+
+  return (
+    <div className="Login">
+      <form className="formlogin">
+        <input type="email" placeholder="E-mail" />
+        <br />
+        <input type="password" placeholder="Password" />
+        <br />
+        <button >
+          <Link to='/Main'>Login</Link>
+        </button>
+        
+      </form>
+    
+    </div>
+
+  );
+}
+
+export default Login;
--- App/src/Navbar.js
+++ App/src/Navbar.js
@@ -1,20 +1,30 @@
 import React from "react";
 
+import H from './Picture/home.png';
+import V from './Picture/video.png';
+import Ma from './Picture/store.png';
+import P from './Picture/people.png';
+import G from './Picture/games.png';
 import './Navbar.css';
 
 function Nav() {
     return (
       <div className="Navbar">
         <div className="L">
-                <p>Lift</p>
+                <p>Lift</p> 
+                <input type="search" placeholder="search" />
         </div>
         <div className="M">
-            <p>Main</p>
-            <p>Main</p>
-            <p>Main</p>
+        
+       <button> <img src={H} className="App-logo" alt="Home" /></button>
+       <button><img src={V} className="App-logo" alt="Video" /></button>
+       <button><img src={Ma} className="App-logo" alt="Market" /></button>
+       <button><img src={P} className="App-logo" alt="People" /></button>
+       <button><img src={G} className="App-logo" alt="Game" /></button>
         </div>
         <div className="R">
             <p>Right</p>
+            <button className="Name" >Pattanawong Kupat</button>
         </div>
       </div>
     );
