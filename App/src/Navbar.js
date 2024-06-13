f00ccc0199fa28a9369dcdb01193ef3eb5535a5f
Pattanawong <166382461+Pattanawong@users.noreply.github.com>
2024-06-14 04:39:19 +0700

    14d05

--- App/src/Navbar.js
+++ App/src/Navbar.js
@@ -8,26 +8,26 @@ import G from './Picture/games.png';
 import './Navbar.css';
 
 function Nav() {
-    return (
-      <div className="Navbar">
-        <div className="L">
-                <p>Lift</p> 
-                <input type="search" placeholder="search" />
-        </div>
-        <div className="M">
-        
-       <button> <img src={H} className="App-logo" alt="Home" /></button>
-       <button><img src={V} className="App-logo" alt="Video" /></button>
-       <button><img src={Ma} className="App-logo" alt="Market" /></button>
-       <button><img src={P} className="App-logo" alt="People" /></button>
-       <button><img src={G} className="App-logo" alt="Game" /></button>
-        </div>
-        <div className="R">
-            <p>Right</p>
-            <button className="Name" >Pattanawong Kupat</button>
-        </div>
+  return (
+    <div className="Navbar">
+      <div className="L">
+        <p>Lift</p>
+        <input type="search" placeholder="search" />
       </div>
-    );
-  }
-  
-  export default Nav;
+      <div className="M">
+
+        <button> <img src={H} className="App-logo" alt="Home" /></button>
+        <button><img src={V} className="App-logo" alt="Video" /></button>
+        <button><img src={Ma} className="App-logo" alt="Market" /></button>
+        <button><img src={P} className="App-logo" alt="People" /></button>
+        <button><img src={G} className="App-logo" alt="Game" /></button>
+      </div>
+      <div className="R">
+        <p>Right</p>
+        <button className="Name" >Pattanawong Kupat</button>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
