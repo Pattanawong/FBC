13be053343faff6f2bdacaf802bca334dcff2924
Pattanawong <166382461+Pattanawong@users.noreply.github.com>
2024-06-19 05:25:59 +0700

    19d05.25

---
+++ App/src/Box.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Ho from './Picture/home.png'
+
+import './Box.css' ;
+
+function Box (){
+  return (
+            <>
+                <div className="Box" ><img src={Ho} /></div>
+            </>
+      
+    
+  );
+
+
+}
+
+export default Box;
--- App/src/Feds.js
+++ App/src/Feds.js
@@ -1,16 +1,24 @@
 import React from "react";
 
 import './Feds.css';
+import Box from "./Box";
 
 function Fed() {
     return (
       <div className="Fed">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
       </div>
     );
   }
